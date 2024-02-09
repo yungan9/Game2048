a98907c29db57491c82cd398478fa5ab42bebe87
yungan9 <geyun105@naver.com>
2024-02-09 23:32:05 +0900

    FEAT:RandomTile

--- game/src/component/Game.js
+++ game/src/component/Game.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import times from "lodash/times";
 import { MAX_POS } from "../constant";
+import { getInitialTileList } from "../util/tile";
 
 export default function Game() {
-  const [tileList, setTileList] = useState([]);
+  const [tileList, setTileList] = useState(getInitialTileList);
   return (
     <div className="game-container">
       <div className="grid-container">
@@ -21,7 +22,7 @@ export default function Game() {
           <div
             className={`tile tile-${item.value} tile-position-${item.x}-${item.y}`}
           >
-            <div className="time-inner">{item.value}</div>
+            <div className="tile-inner">{item.value}</div>
           </div>
         ))}
       </div>
