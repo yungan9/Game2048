29d63ddc42c8a565a98da95c7cde6bbaffeb2cc4
yungan9 <geyun105@naver.com>
2024-03-18 18:02:26 +0900

    DESIGN: Layout

--- game/src/component/AboveGame.js
+++ game/src/component/AboveGame.js
@@ -4,11 +4,8 @@ export default function AboveGame() {
   return (
     <div className="above-game">
       <p className="game-intro">
-        Join the tiles, get to <strong>2048!</strong>
+        HI, ToyProject <strong>2048!</strong>
       </p>
-      <a href="javascript:void(0)" className="how-to-play-link">
-        How to play →
-      </a>
       <a href="/" className="restart-button">
         New Game
       </a>
